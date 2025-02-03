1af7ed47ee4ed1847c1377924f1b150ccdf06b5a
EdgeworthLau <edgeworthlau@outlook.com>
2025-02-03 19:22:41 +0800

    Add copy button

---
+++ assets/js/copy.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+  let codeBlocks = document.getElementsByTagName('code');
+
+  for (let i = 0; i < codeBlocks.length; i++) {
+    let codeBlock = codeBlocks[i];
+
+    // 创建复制按钮
+    let button = document.createElement('button');
+    button.innerHTML = '<i class="fa fa-camera-retro fa-lg" aria-hidden="true"></i>';
+    button.classList.add('copy-button'); // 添加 CSS 类
+
+    // 将按钮插入到 <code> 元素后面
+    codeBlock.parentNode.insertBefore(button, codeBlock.nextSibling);
+
+    // 初始化 Clipboard.js
+    let clipboard = new ClipboardJS(button, {
+      target: function (trigger) {
+        return trigger.previousSibling;
+      }
+    });
+    // 复制失败事件
+    clipboard.on('error', function (e) {
+      console.error('复制失败:', e.action);
+    });
+  }
+});
